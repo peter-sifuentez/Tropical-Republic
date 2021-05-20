6db386e5ae3f624bd0bc69dd6b01dcbd103f88ce
Peter Sifuentez <sifuentez_peter@yahoo.com>
2021-05-19 21:53:07 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
+/*Headliner*/
 function myFunction() {
   var x = document.getElementById('myTopnav');
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
+  }
+}
+
+/*Login Modal*/
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
   }
 }
